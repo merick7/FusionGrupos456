6ece196f6bb0320a0f8cf4120b9200bb50dd933b
Javier0821 <javier_6085@hotmail.com>
2021-10-16 20:43:28 -0500

    Prueba 3

--- WebGanadera.App/WebGanadera.App.Consola/Program.cs
+++ WebGanadera.App/WebGanadera.App.Consola/Program.cs
@@ -36,7 +36,7 @@ namespace WebGanadera.App.Consola
             var ganadero = new Ganadero{
                 //Id=123,
                 TipoId="Cedula",
-                Nombres="Andrea",
+                Nombres="Carlos Javier",
                 Apellidos="Rengifo Mendez",
                 NumeroTel = "3155667478",
                 Direccion = "Casa campo 1234",
