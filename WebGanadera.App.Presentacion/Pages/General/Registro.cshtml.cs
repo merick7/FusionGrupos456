d9e4e2d820240f3be66f89e3682db3888e401a8b
Javier0821 <javier_6085@hotmail.com>
2021-10-18 09:02:35 -0500

    AgregarBD

--- WebGanadera.App.Presentacion/Pages/General/Registro.cshtml.cs
+++ WebGanadera.App.Presentacion/Pages/General/Registro.cshtml.cs
@@ -4,13 +4,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebGanadera.App.Dominio;
+using WebGanadera.App.Persistencia;
 
 namespace WebGanadera.App.Presentacion.Pages
 {
     public class RegistroModel : PageModel
     {
+        private static IRepositorioGanadero _repoGanadero = new RepositorioGanadero(new Persistencia.AppContext());
+        private static IRepositorioVeterinario _repoVeterinario = new RepositorioVeterinario(new Persistencia.AppContext());
+        public IEnumerable<Ganadero> ganaderos { get; private set;}
+        public IEnumerable<Veterinario> veterinarios { get; private set;}
+        public List<Ganadero> Ganaderos { get; set;}
         public void OnGet()
         {
+            ganaderos = _repoGanadero.ObtenerTodosGanaderos();
+        }
+
+        public void OnPostAgregar(Ganadero ganadero, Veterinario veterinario){
+            if (ganadero != null ) //&& veterinario.NumLicencia == null && veterinario.Especialidad == null
+            {
+                _repoGanadero.AgregarGanadero(ganadero);
+            }
+            else if(veterinario != null) //&& veterinario.NumLicencia == null && veterinario.Especialidad == null
+            {
+                _repoVeterinario.AgregarVeterinario(veterinario);
+            }
+            ganaderos = _repoGanadero.ObtenerTodosGanaderos();
+            veterinarios = _repoVeterinario.ObtenerTodosVeterinarios();
         }
     }
 }
